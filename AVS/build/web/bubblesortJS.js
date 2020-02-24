e1f1026753c805b7ea4d045be2dc65906c9dad6a
quangnhse05858 <59592341+quangnhse05858@users.noreply.github.com>
2020-02-24 21:21:34 +0700

    Update AVS 24/2/2020
    -Thay file index.html bằng index.jsp
    - Thay đổi speed value
    - tại index.jsp thêm code highlight
    - Chưa có biểu đồ do chưa kéo file servlet và 1 số library sang đây

--- AVS/build/web/bubblesortJS.js
+++ AVS/build/web/bubblesortJS.js
@@ -1,12 +1,13 @@
-
-N = 10; // Array Size
-XYs = 5; // Element Visual Size
-Xp = 1; // Start Pos X
-Yp = 1; // Start Pos Y
+var initArray = [9, 8, 7, 6, 9, 4, 3, 2, 1, 1];
+//export var initArray = [9, 8, 7, 6, 9, 4, 3, 2, 1, 1];
+var N = 10; // Array Size
+var XYs = 5; // Element Visual Size
+var Xp = 1; // Start Pos X
+var Yp = 1; // Start Pos Y
 var canvas;
 var currentstep = 0;
 var totalstep;
-var initArray = [9, 8, 7, 6, 9, 4, 3, 2, 1];
+
 var run;
 var boolRun = true;
 var arr_by_user = [];
@@ -14,6 +15,13 @@ var speed = 1000;
 var eachStepArr = [];
 var highlight = [];
 var color = [];
+//window.init = init;
+//window.shuffle = shuffle;
+//window.initArray = initArray;
+//window.next = next;
+//window.back = back;
+//window.resume = resume;
+
 
 function shuffle() {
     var oldPos = initArray.length,
@@ -48,7 +56,7 @@ function integerArray(stringArr) {
 
 function inputByUser() {
     arr_by_user = document.getElementById("txtElement").value;
-    arr_by_user = arr_by_user.split('.');
+    arr_by_user = arr_by_user.split(',');
     if (integerArray(arr_by_user) !== null) {
         initArray = integerArray(arr_by_user);
     }
@@ -56,6 +64,25 @@ function inputByUser() {
 }
 
 function init() {
+     var mydata = JSON.stringify(initArray);
+    console.log("datanormal: " + initArray);
+    $.ajax({
+        type: "POST",
+        url: "SortStepServlet",
+        data: {name: mydata}
+        ,
+        dataType: "json",
+        //OK
+        success: function (data) {
+            console.log(data);
+            drawGraph(data);      
+        }
+        ,
+        error: function (error) {
+            console.log("error", error);
+        }
+    }
+    );
     bubbleSort2(initArray);
     currentstep = -1;
     canvas = document.getElementById('canvas');
@@ -64,6 +91,7 @@ function init() {
         clearInterval(run);
         loadingAnimation();
     }
+   
 }
 
 function next() {
@@ -121,9 +149,9 @@ function loadingAnimation() {
     run = setTimeout(loadingAnimation, speed);
 }
 
-
 //ve ra lai cai mang
 function draw(currentstep) {
+    
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         for (var i = 0; i < eachStepArr[currentstep].length; i++) {
@@ -144,7 +172,16 @@ function draw(currentstep) {
                     ctx.fillText(eachStepArr[currentstep][i - 1], (Xp * (i - 1) * 16) * XYs + 10, Yp * XYs * 5, 60);
                 }
             }
+            if(highlightcode[i]!==0) {
+            var line_name= "line_" + highlightcode[i];
+             document.getElementById(line_name).style.background="None";
+         }
         }
+        if(highlightcode[currentstep]!==0) {
+            var line_name= "line_" + highlightcode[currentstep];
+             document.getElementById(line_name).style.background="Red";
+         }
+        
     }
     if (currentstep === totalstep) {
         document.getElementById("btnNext").disabled = true;
@@ -170,9 +207,10 @@ function newarray(array) {
 
     return newarray;
 }
-
-function bubbleSort2(array) { // * is magic   
-    temparray = newarray(array);
+var highlightcode= [];
+function bubbleSort2(array) { // * is magic  
+    
+    var temparray = newarray(array);
     var count = 0;
     eachStepArr = [];
     highlight = [];
@@ -180,38 +218,96 @@ function bubbleSort2(array) { // * is magic
     eachStepArr.push(newarray(temparray));
     highlight.push(0);
     color.push(0);
+    highlightcode.push(1);
     for (var i = 0; i < temparray.length - 1; i++) {
         for (var j = 0; j < temparray.length - i - 1; j++) {
             count++;
             eachStepArr.push(newarray(temparray));
             highlight.push(j);
             color.push("normal");
-            if (temparray[j] > temparray[j + 1]) {
+          highlightcode.push(2);
+            if (temparray[j] > temparray[j + 1]) {             
                 var temp = temparray[j];
                 temparray[j] = temparray[j + 1];
                 temparray[j + 1] = temp;
                 color.push("swap");
                 eachStepArr.push(newarray(temparray));
                 highlight.push(j);
+                highlightcode.push(3);
             }
         }
     }
+    highlightcode.push(9);
     eachStepArr.push(newarray(temparray));
     totalstep = eachStepArr.length - 1;
     document.getElementById("txtStepcount").innerHTML = '' + (currentstep + 1) + "/" + (totalstep + 1);
     document.getElementById("progressStep").max = eachStepArr.length - 1;
     document.getElementById("slideStep").max = eachStepArr.length - 1;
+    
 }
 
 //chinh speed khi user giu con tro chuot
 function changeSpeed() {
-    var spMaxValue = parseInt(document.getElementById("rangebar").max);
-    var spMinValue = parseInt(document.getElementById("rangebar").min);
-    var spValue = parseInt(document.getElementById("rangebar").value);
-    speed = Math.abs(spValue - (spMaxValue + spMinValue));
+    speed = 1000/(parseInt(document.getElementById("rangebar").value));
+  
 }
 
 //chưa sửa được lỗi
+
+
+function drawGraph(data) {
+    var my_canvas = document.getElementById("canvas2");
+    var gctx = my_canvas.getContext("2d");
+
+//cái data này lúc sau sẽ là Get và json convert
+//var data = [['Bubble Sort', 140], ['Selection sort', 150], ['Quick Sort', 170], ['Heap Sort', 110], ['Insertion sort', 170]];
+
+///////// Settings  ////////// 
+
+    var bar_width = 50;
+    var y_gap = 30;  // Gap below the graph 
+    var bar_gap = 100; // Gap between Bars including width of the bar
+    var x = 20; // Margin of graph from left  
+
+    var y = my_canvas.height - y_gap;
+
+    my_canvas.width = data.length * (bar_gap) + x;
+////////////end of settings ////
+    gctx.moveTo(x - 5, y);
+    gctx.lineTo(my_canvas.width, y); // Base line of graph 
+    gctx.stroke();
+/// add shadow ///
+    gctx.shadowColor = '#000000';
+    gctx.shadowOffsetX = 3;
+    gctx.shadowOffsetY = 3;
+    gctx.shadowBlur = 3;
+/////////// Draw the graph ////////
+
+    for (var i = 0; i < data.length; i++) {
+        gctx.shadowColor = '#ffffff'; // remove this line if you shadow on text is required
+        gctx.font = '18px serif'; // font for base label showing classes 
+        gctx.textAlign = 'left';
+        gctx.textBaseline = 'top';
+        gctx.fillStyle = '#008cf5';
+        gctx.fillText(data[i].sortName, x, y + 5); // Write base text for classes 
+
+        gctx.beginPath();
+        gctx.lineWidth = 2;
+        var y1 = y - data[i].number_of_step; // Coordinate for top of the Bar 
+        var x1 = x;
+        gctx.font = '12px serif'; // font at top of the bar 
+        gctx.fillStyle = '#000000';
+        gctx.fillText(data[i].number_of_step, x1, y1 - 20); // text at top of the bar 
+
+        gctx.fillStyle = '#f52369'; // fill Colur of bar  
+        gctx.shadowColor = '#000000'; // shadow color for bars 
+        gctx.fillRect(x1, y1, bar_width, data[i].number_of_step);// Filled bar 
+
+        x = x + bar_gap;
+
+    }
+}
+
 
 function setInputFilter(textbox, inputFilter) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
@@ -238,8 +334,6 @@ function setInputFilter(textbox, inputFilter) {
 setInputFilter(document.getElementById("intLimitTextBox"), function (value) {
     return /^(\d+[, ]+)*$/.test(value);
 });
-
-
 
 
 
---
+++ AVS/web/index.jsp
@@ -0,0 +1,51 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Algorithm compare</title>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script type="text/javascript" src="bubblesortJS.js.js"></script>
+
+    </head>
+
+    <body onload ="init();">
+        <h1>Hello ! jsp 1</h1>  
+        <progress id="progressStep" value="1"  max="100" > 0 </progress>
+        <p>Step</p>
+        <input id="slideStep" type="range" oninput="chooseStep();" max="0" min="0" value="1">
+        <div id="txtStepcount" style="float: contour"></div>
+        <input type="submit" onclick=" init();" value="Restart" />    
+        <input id="PauseOrCon" type="submit" onclick="resume();" value="Pause" />
+        <input id="btnPrev" type="submit" onclick="back();" value="<" />
+        <input id="btnNext" type="submit" onclick="next();" value=">" />
+        <input id="txtElement" onkeydown="isNumberKey(event);" type="text" name="name" value="" />
+        <input type="submit" onclick="inputByUser();" value="Nhập data" />
+        <input id="btnShuffle" type="submit" onclick="shuffle();" value="Shuffle" />
+        <canvas id="canvas" width="700" height="50" style="border:1px solid #000000;"></canvas>
+        <canvas id="canvas2" width="35" height="230"  style="border:2px solid #000000;"></canvas>
+        <p>Speed</p>
+        <input id="rangebar" type="range" oninput="changeSpeed();" max="10" min="1" value="1">    
+        <script type="text/javascript" src="jsonjs.js"></script>
+        
+        <div>            
+        <table>        
+            <tr id="line_1"> <td> for (var i = 0; i < temparray.length - 1; i++)  { </td> </tr>
+            <tr id="line_2"> <td>for (var j = 0; j < temparray.length - i - 1; j++) {  </td>  </tr>    
+            <tr id="line_3"> <td>  if (temparray[j] > temparray[j + 1]) { </td></tr>
+            <tr id="line_4"><td> &nbsp;  var temp = temparray[j]; </td></tr>
+            <tr id="line_5"> <td>   &nbsp;  temparray[j] = temparray[j + 1];</td></tr>
+            <tr id="line_6"> <td>  &nbsp;      temparray[j + 1] = temp;  </td>  </tr>  
+            <tr id="line_7"> <td>   }</td></tr>
+            <tr id="line_8"><td>   }</td></tr>
+            <tr id="line_9"> <td>  } </td></tr>          
+        </table>
+        </div>
+    
+
+</body>      
+
+</html>
