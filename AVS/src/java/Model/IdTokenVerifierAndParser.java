a3fc3bd1ce63710fc6558dd2c78978c267673fb9
binhntb <60654160+binhntb@users.noreply.github.com>
2020-03-17 00:03:59 +0700

    đăng nhập google
    -update login theo google acount
    -thêm trang token verifier
    -thêm signin.js và signout.js
    -update header và login.jsp

---
+++ AVS/src/java/Model/IdTokenVerifierAndParser.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+/**
+ *
+ * @author BinhNT
+ */
+public class IdTokenVerifierAndParser {
+      private static final String GOOGLE_CLIENT_ID = "912620741556-58chb0o18k50h03oblr4gr5t8vrqi139.apps.googleusercontent.com";
+
+    public static GoogleIdToken.Payload getPayload(String tokenString) throws Exception {
+
+        JacksonFactory jacksonFactory = new JacksonFactory();
+        GoogleIdTokenVerifier googleIdTokenVerifier
+                = new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
+        GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+        if (googleIdTokenVerifier.verify(token)) {
+            GoogleIdToken.Payload payload = token.getPayload();
+            if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
+                throw new IllegalArgumentException("Audience mismatch");
+            } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
+                throw new IllegalArgumentException("Client ID mismatch");
+            }
+            return payload;
+        } else {
+            throw new IllegalArgumentException("id token cannot be verified");
+        }
+    }
+}
